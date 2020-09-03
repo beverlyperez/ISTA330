a2d212c006b2c002b2ab7fdceff9a04cd0a13932
beverlyperez <42483610+beverlyperez@users.noreply.github.com>
2020-09-03 03:59:41 -0700

    fixed up q6

--- practicum/week-1/question5.js
+++ practicum/week-1/question5.js
@@ -12,5 +12,11 @@ output: 3
 */
 
 var minimalReduction = function(n) {
-
+    var numToDivBy;
+    for(var i = 0; i < n.length; i++){
+        if(n % i == 0){
+            numToDivBy = i;
+        }
+    }
+    
 };
--- practicum/week-1/question6.js
+++ practicum/week-1/question6.js
@@ -6,5 +6,15 @@ output: 3
 */
 
 var median = function(input) {
-
+    var med;
+    var lenOfIn = input.length;
+    if(lenOfIn % 2 != 0){
+        med = input[Math.floor(lenOfIn/2)];
+    }
+    else{
+        med = (input[Math.floor(lenOfIn/2)] + input[Math.ceil(lenOfIn/2)])/2;
+    }
+    return med;
 };
+var output = median([1, 2, 2, 3, 4, 7, 9]);
+console.log(output);
