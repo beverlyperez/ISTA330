1a991b3933b0c30a06f1b3b5da98d28bf15fc9ab
beverlyperez <42483610+beverlyperez@users.noreply.github.com>
2020-09-03 21:20:18 -0700

    should have finished q8

--- practicum/week-1/question8.js
+++ practicum/week-1/question8.js
@@ -8,5 +8,21 @@ output: [1, 0, 1, 2]
 */
 
 var biggerAndEven = function(input) {
-
+    var timesThroughLoop = 0;
+    var returnList = [];
+    for(var j = 0; j < input.length; j++){
+        returnList[j] = 0;
+    }
+    console.log(returnList);
+    for(var i = 0; i < input.length; i++){
+        for(var j = 0; j < input.length; j++){
+            if(input[j] > input[i] && input[j] % 2 == 0){
+                returnList[i] += 1;
+            }
+        }
+    }
+    return returnList;
 };
+var output = biggerAndEven([1,3,4,2,6,8,5,7]);
+
+console.log(output);
