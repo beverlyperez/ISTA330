58cfa19d549193f6a5de7610c98fba9ed2106c7f
beverlyperez <42483610+beverlyperez@users.noreply.github.com>
2020-09-04 21:11:48 -0700

    finished q10

--- practicum/week-1/question10.js
+++ practicum/week-1/question10.js
@@ -8,5 +8,24 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
+   var tempN = String(n);
+    var maxInN = 0;
+    var minInN = 0;
 
-};
+    for(var i = 0; i < tempN.length; i++){
+        if(i == 0){
+            minInN = tempN[i];
+        }
+        if(maxInN < tempN[i]){
+            maxInN = tempN[i];
+        }
+        if(minInN > tempN[i]){
+            minInN = tempN[i];
+        }
+
+    }
+     var differenceMaxMin = maxInN - minInN;
+    return differenceMaxMin;
+};   
+var output = maxMinusMin(87034);
+ console.log(output);
