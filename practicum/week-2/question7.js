cb818cd55def0ab3a5442803b4c88007d42ea5d1
beverlyperez <42483610+beverlyperez@users.noreply.github.com>
2020-09-07 17:06:30 -0700

    finished q7

--- practicum/week-2/question7.js
+++ practicum/week-2/question7.js
@@ -12,5 +12,25 @@ output: [[1,4], [2,5], [3,6]]
 */
 
 var transpose = function(M) {
+    var finalList = [];
+    var insideLen = 0;
+    var keepTrack = 0; 
+    for(var k = 0; k < M[0].length; k++){
+        finalList[k] = [];
 
+    }
+    for(var i = 0; i < M.length; i++){
+
+        for(var j = 0; j < M[i].length; j++){
+            finalList[keepTrack][insideLen] = M[i][j];
+            keepTrack++;
+
+            if(keepTrack == 3){
+                insideLen++;
+            }
+        }
+        keepTrack = 0;
+    }
+
+    return finalList;
 };
