26445c744661d1e2acb5131214f57a39ab0b37b7
beverlyperez <42483610+beverlyperez@users.noreply.github.com>
2020-09-17 12:18:51 -0700

    finished w2 hopefully

--- practicum/week-2/question11.js
+++ practicum/week-2/question11.js
@@ -10,9 +10,30 @@ where |input1[i]-input2[j]| <= d.
 
 Example:
 input: input1 = [2,1,26,3], input2 = [-5,-2,10,-3,7], d = 6
+26 is the only element that works 
 output: 1
 */
 
 var d_count = function(input1, input2, d) {
-
+    var dCount = 0;
+    var tOfF = false;
+    for(var i = 0; i < input1.length; i++){
+        for(var j = 0; j < input2.length; j++){
+            
+            if( Math.abs(input1[i] - input2[j]) > Number(d) ){
+                tOfF = true;
+            }
+            else{
+                tOfF = false;
+            }
+            
+        }
+        if(tOfF == true){
+            dCount++;
+        }
+        tOfF = false;
+    }
+    return dCount;
 };
+var output = d_count([2,1,26,3], [-5,-2,10,-3,7], 6);
+console.log(output);
--- practicum/week-2/question9.js
+++ practicum/week-2/question9.js
@@ -8,10 +8,43 @@ Return how many groups have the largest size.
 Example:
 input: n = 11
 output: 2
-The groups are: [1, 10], [2, 11], [3], [4], [5], [6], [7], [8], [9]
+The groups are: [1, 10], [2, 11], [3, 12], [4. 13], [5, 14], [6, 15], [7], [8], [9]
 so there are two groups with the largest size.
 */
 
 var largestGroupsCount = function(n) {
+    var finalList = [];
+    var keepTrackForkeepTrack = 0;
+    var keepTrack = 1;
+    var maxLen = 0;
+    for(var j = 0; j < 9; j++){
+        finalList[j] = [];
+    }
+    for(var i = 0; i < n; i++){
+        if(i >= 9){
+            finalList[i - 9][keepTrack] = i + 1;
+            keepTrackForkeepTrack++;
+            if(keepTrackForkeepTrack == 9){
+                keepTrack++;
+                keepTrackForkeepTrack = 0;
+            }
+            
+        }
+        else{
+            finalList[i][0] = i + 1;
+        }
 
+    }
+    for(var l = 0; l < finalList.length; l++){
+        if(finalList[l].length > maxLen){
+            maxLen = finalList[l].length;
+        }
+    }
+    var lensOfMax = 0;
+    for(var l = 0; l < finalList.length; l++){
+        if(finalList[l].length == maxLen){
+            lensOfMax++;
+        }
+    }
+    return lensOfMax;
 };
