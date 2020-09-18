919ba4b9130c9f3ea88bb90a02171840e493f1fd
beverlyperez <42483610+beverlyperez@users.noreply.github.com>
2020-09-18 04:34:35 -0700

    finsihed q5

--- practicum/week-3/question5.js
+++ practicum/week-3/question5.js
@@ -10,6 +10,20 @@ output: [24,12,8,6]
 */
 
 var productOfOthers = function(input) {
+    var outputList = [];
+    var keepTrack = 0;
+    for(var i = 0; i < input.length; i++){
+        var finProduct = 1;
+
+        for(var j = 0; j < input.length; j++){
+            if(i != j){
+                finProduct = finProduct * input[j];
+            }
+        }
+        outputList[i] = finProduct;
+    }
+    return outputList;
 
 };
-
+var output = productOfOthers([1, 2, 3, 4]);
+console.log(output);
