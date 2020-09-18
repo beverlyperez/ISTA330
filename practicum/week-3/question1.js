274a9537281f619547e96bb188a5d8f60f1a6853
beverlyperez <42483610+beverlyperez@users.noreply.github.com>
2020-09-18 06:53:18 -0700

    fixed q1

--- practicum/week-3/question1.js
+++ practicum/week-3/question1.js
@@ -18,12 +18,29 @@ Return the number of teams you can form given the conditions.
          We can only form one team given the 
          conditions: (2,3,4)
 */
+function findMax(input){
+        var returnMax = input[0];
+        for(var i = 0; i < input.length; i++){
+                if(input[i] > returnMax){
+                        returnMax = input[i];
+                }
+        }
+        return returnMax;
+}
+function isSorted(input){
+        
+        for(var i = 1; i < input.length; i++){
+                if(input[i - 1] > input[i]){
+                        return false;
+                }
+        }
+        return true;
+}
 
 var howManyTeams = function(input) {
         var tempList = [];
         var keepTrack = 0;
         var count = 0;
-        var tempVal = 0;
         var minNum;
         var runThrough = 0;
         var j = 1;
@@ -31,13 +48,11 @@ var howManyTeams = function(input) {
         var maxNum = findMax(input);
         for (var i = 0; i < input.length - 3; i++){
                 tempList[keepTrack] = input[i];
-                console.log(input[i] + " input at i = " + i);
                 keepTrack++;
                 minNum = input[i];
 
                 while(runThrough != 2){
-                        console.log(tempList[keepTrack - 1] + " templist");
-                        console.log(numToAdd+ "  number to add");
+                      
                         if((tempList[keepTrack - 1] + numToAdd) == maxNum){
                                 j++;
                                 numToAdd = 0;
@@ -54,9 +69,7 @@ var howManyTeams = function(input) {
                                 numToAdd++;
                         }
                 }
-                console.log(tempList + " this is templist");
                 if(tempList.length == 3 && isSorted(tempList)){
-                        console.log(tempList + "there is a templist ");
                         count++;
                 }
                 keepTrack = 0;
@@ -64,23 +77,5 @@ var howManyTeams = function(input) {
         return count;
 };
 
-function findMax(input){
-        var returnMax = input[0];
-        for(var i = 0; i < input.length; i++){
-                if(input[i] > returnMax){
-                        returnMax = input[i];
-                }
-        }
-        return returnMax;
-}
-function isSorted(input){
-        
-        for(var i = 1; i < input.length; i++){
-                if(input[i-1] > input[i]){
-                        return false;
-                }
-        }
-        return true;
-}
-var output = howManyTeams([2,5,1]);
-console.log(output + "   this is output");
+//var output = howManyTeams([2,5,3,4,1]);
+//console.log(output + "   this is output");
